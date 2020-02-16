5e5883b39e1ccbcecd85aa8baea4365996cf6b7a
cevdettimk <cevdet.timk@ogr.dpu.edu.tr>
2020-02-17 00:52:59 +0300

    Lesson3

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { templateJitUrl } from '@angular/compiler';
+import {Movie} from '../movie';
 
-
-@Component({
-    selector :'movies' ,// <movies></movies>
+//@Component({
+    //selector :'movies' ,// <movies></movies>
     //selector :'.movies' // <div class ="movies"></movies> nokta başına konulduğunda class şeklinde tanımlanır
     //selector :'#movies' // <div id ="movies"></movies> # olduğunda id olduğunu belirtir
     //template: '<h2>Movies</h2>'
-    templateUrl: 'movies.component.html'
-})
-export class MoviesComponent{
+    //templateUrl: './movies.component.html',
+    //styleUrls: ['./movies.component.css']
 
+//})
+
+@Component({
+    selector: 'movies',
+    // template:`
+    // <h2>  {{ "Title" + getTitle() }}</h2> 
+    // <div> {{movie.id}} </div>
+    // <div> {{movie.name}} </div>
+    // `
+    templateUrl: './movies.component.html',
+    //styleUrls: ['./movies.component.css']
+  })
+
+export class MoviesComponent{
+  title ='Movies List'
+movie:Movie ={
+id : 1,
+name :'Movie List'
+}
+  getTitle()
+  {
+    return this.title;
+  }
 }
