7bffe033f76ea75880e6c98c338faeccceeb0922
cevdettimk <cevdet.timk@ogr.dpu.edu.tr>
2020-02-23 13:17:21 +0300

    Logging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieComponent } from './movie/movie.component';
+import { LoggingComponent } from './logging/logging.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
-    MovieComponent
+    MovieComponent,
+    LoggingComponent
   ],
   imports: [
     BrowserModule
